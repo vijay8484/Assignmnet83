2fb0a47d22f67814346ad30fec53456530a9ef75
Your NameVijay <vijay8484patil@gmail.com>
2019-01-04 23:34:22 +0530

    Assigment 83

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Assignment81';
+
+  public UserName="";
+  title = 'app';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// This import is required for ngModel
+import {FormsModule} from '@angular/forms';
+
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule       // Add explicite import for this
   ],
   providers: [],
   bootstrap: [AppComponent]
